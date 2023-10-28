7fae923197f76d6c5a090a95ad7b108cfdc6f77c
ROBERT II <24195153+Harlocks@users.noreply.github.com>
2023-10-28 13:26:16 +0000

    lib: colors

---
+++ bin/akfetch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# -----------------------------------
+# Autor : Erick Robert
+# Email : h4rt3ck@pm.me
+# GitHub: https://github.com/harlocks
+# -----------------------------------
+# Data  : 27/10/2023
+# -----------------------------------
+# Script que mostra todas as informações
+# do sistema de forma organizada. Com algumas
+# ferramentas a mais de personalização.
+# -----------------------------------
+clear
+
+# --- IMPORTS
+
+. /etc/os-release
+
+# --- COLORS
+
+blackB="$(tput setab 0)"
+blackF="$(tput setaf 0)"
+redB="$(tput setab 1)"
+redF="$(tput setaf 1)"
+greenB="$(tput setab 2)"
+greenF="$(tput setaf 2)"
+yellowB="$(tput setab 3)"
+yellowF="$(tput setaf 3)"
